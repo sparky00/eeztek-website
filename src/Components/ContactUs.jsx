678f5ef0eea6fedf94d2613bd92c87d5eb576e47
Rahul Mishra <mishrarahul1919@gmail.com>
2023-11-04 19:09:03 +0530

    fixed contact us page

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -94,10 +94,79 @@ const ContactUs = () => {
                 <br />
                 {/* <span className="text-primary">for your business</span> */}
               </h1>
-
+            </div>
+            <div className="mb-12 lg:mb-0">
+              <form class="-m-2 flex flex-wrap" onSubmit={submitHandler}>
+                <div class="w-1/2 p-2">
+                  <div class="relative">
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      class="peer w-full rounded border border-gray-700 bg-gray-800 bg-opacity-40 py-1 px-3 text-base leading-8 text-gray-100 placeholder-transparent outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900"
+                      placeholder="Name"
+                    />
+                    <label
+                      for="name"
+                      class="absolute left-3 -top-6 bg-transparent text-sm leading-7 text-white-600 transition-all peer-placeholder-shown:left-3 peer-placeholder-shown:top-2  peer-placeholder-shown:text-base text-white-600 peer-focus:left-3 peer-focus:-top-6 peer-focus:text-sm peer-focus:text-white-500"
+                    >
+                      Name
+                    </label>
+                  </div>
+                </div>
+                <div class="w-1/2 p-2">
+                  <div class="relative">
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      class="peer w-full rounded border border-gray-700 bg-gray-800 bg-opacity-40 py-1 px-3 text-base leading-8 text-gray-100 placeholder-transparent outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900"
+                      placeholder="Email"
+                    />
+                    <label
+                      for="email"
+                      class="absolute left-3 -top-6 bg-transparent text-sm leading-7 text-white-600 transition-all peer-placeholder-shown:left-3 peer-placeholder-shown:top-2  peer-placeholder-shown:text-base text-white-600 peer-focus:left-3 peer-focus:-top-6 peer-focus:text-sm peer-focus:text-white-500"
+                    >
+                      Email
+                    </label>
+                  </div>
+                </div>
+                <div class="mt-4 w-full p-2">
+                  <div class="relative">
+                    <textarea
+                      id="message"
+                      name="message"
+                      value={message}
+                      onChange={(e) => setMessage(e.target.value)}
+                      class="peer h-32 w-full resize-none rounded border border-gray-700 bg-gray-800 bg-opacity-40 py-1 px-3 text-base leading-6 text-gray-100 placeholder-transparent outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900"
+                      placeholder="Message"
+                    ></textarea>
+                    <label
+                      for="message"
+                      class="absolute left-3 -top-6 bg-transparent text-sm leading-7 text-white-600 transition-all peer-placeholder-shown:left-3 peer-placeholder-shown:top-2  peer-placeholder-shown:text-base text-white-600 peer-focus:left-3 peer-focus:-top-6 peer-focus:text-sm peer-focus:text-white-500"
+                    >
+                      Message
+                    </label>
+                  </div>
+                </div>
+                <div class="p-2">
+                  <button
+                    type="submit"
+                    class="mx-auto flex rounded border-0 bg-black py-2 px-8 text-lg text-white hover:bg-indigo-950 focus:outline-none"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
-
 export default ContactUs;
-
-// git remote add origin https://github.com/sparky00/eeztek-website.git
